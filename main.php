e8d7b3d3793bf43c90586d50159df0908985979b
MustafaCanBasaran <mcanbasaran@gmail.com>
2018-02-08 23:47:31 +0300

    Main Page

---
+++ main.php
@@ -0,0 +1,19 @@
+<?php require_once "include/header_html.php"; ?>
+
+<body class="fixed-nav sticky-footer bg-dark" id="page-top">
+
+<?php require_once "include/nav_html.php"; ?>
+
+  <div class="content-wrapper">
+    <div class="container-fluid">
+   		<h2>Aquarium assistant</h2>
+   		<li><b>Feature 1 :</b> Volume Calculation</li>
+    </div>
+   
+    <?php require_once "include/footer_html.php"; ?>
+
+
+  </div>
+</body>
+
+</html>
